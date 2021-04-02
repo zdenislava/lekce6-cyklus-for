357b44a1692936c807ef5228a4c0836f3c2b713d
karolyim <matej.karolyi@gmail.com>
2021-04-02 16:36:52 +0200

    [MAINTENANCE] project init

---
+++ main.js
@@ -0,0 +1,31 @@
+// Použij výpis do konzole prohlížeče pomocí console.log('text')
+// Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
+
+
+// První příklad - vypiš vzestupně čísla od 0 do 10
+console.log('První příklad - vzestupná řada 0 - 10');
+
+//TADY NAPIS CYKLUS
+
+console.log('-------------------');
+
+
+
+// Druhý příklad - zkopíruj předchozí příklad
+// a vyzkoušej, jaký je rozdíl mezi <= a <
+// v podmínce uvnitř cyklu
+
+console.log('Druhý příklad - rozdíl mezi <= a <');
+
+//TADY NAPIS CYKLUS
+
+console.log('-------------------');
+
+
+
+// Třetí příklad - vypiš sestupně čísla od 10 do 0
+console.log('Třetí příklad - sestupná řada 10 - 0');
+
+//TADY NAPIS CYKLUS
+
+console.log('-------------------');
