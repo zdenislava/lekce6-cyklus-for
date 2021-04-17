c171f6d95636213ad249b3a17b8333d25ab12332
zdenislava <navarikova@seznam.cz>
2021-04-17 19:26:07 +0200

    while

--- main.js
+++ main.js
@@ -4,28 +4,15 @@
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('Prvni příklad');
-/* let cisla = [];
 
-for (let i = 0; i <= 10; i = i + 1) {
-    cisla.push(i);
-};
-
-console.log(cisla); */
 let i = 0;
 
 while (i <= 10){
     console.log(i++);
-}
+};
 
 
 //TADY NAPIS CYKLUS
-
-
-//let odstavec = document.querySelector(".cisla");
-
-/* for (let i = 0; i < cisla.length; i++) {
-odstavec.innerHTML += cisla[i] + " ";
-}; */
 
 
 // Druhý příklad - zkopíruj předchozí příklad
@@ -42,16 +29,6 @@ while (p < 10){
 
 //TADY NAPIS CYKLUS
 
-/* let cislicka = [];
-
-for (let i = 0; i < 10; i = i + 1) {
-    cislicka.push(i);
-}
-console.log(cislicka); */
-
-
-
-
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
@@ -63,15 +40,3 @@ while (d >= 0){
 
 //TADY NAPIS CYKLUS
 
-//let cislaDolu = [];
-
-/* for (let i = 10; i >= 0; i = i - 1) {
-    cislaDolu.push(i);
-}
-console.log(cislaDolu);
-
-let sestupnaRada = document.querySelector(".radaDolu");
-
-for (let i = 0; i < cislaDolu.length; i++) {
-    sestupnaRada.innerHTML += cislaDolu[i] + " ";
-}; */
